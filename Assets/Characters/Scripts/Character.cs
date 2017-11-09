17440dc9e895d32c2bece904054eec6a7d29faf9
Alice Robinson <valkrysa@gmail.com>
2017-11-08 23:03:58 -0800

    fixed a major sliding bug

--- Assets/Characters/Scripts/Character.cs
+++ Assets/Characters/Scripts/Character.cs
@@ -16,13 +16,12 @@ namespace RPG.Characters {
 		[SerializeField] private float audioSourceSpatialBlend = 0.5f;
 
 		[Header("Capsule Collider")]
-		[SerializeField] private Vector3 colliderCenter;
-		[SerializeField] private float colliderRadius;
-		[SerializeField] private float colliderHeight;
-		[SerializeField] private PhysicMaterial physicMaterial;
+		[SerializeField] private Vector3 colliderCenter = new Vector3(0, 1.03f, 0);
+		[SerializeField] private float colliderRadius = 0.2f;
+		[SerializeField] private float colliderHeight = 2.03f;
 
 		[Header("Movement")]
-		[SerializeField] private float moveSpeedMultiplier = 1.5f;
+		[SerializeField] private float moveSpeedMultiplier = .7f;
 		[SerializeField] private float animationSpeedMultiplier = 1.5f;
 		[SerializeField] private float moveThreshold = 1f;
 		[SerializeField] private float movingTurnSpeed = 360;
@@ -47,9 +46,7 @@ namespace RPG.Characters {
 		private void Update() {
 			if (!navMeshAgent.isOnNavMesh) {
 				Debug.LogError(gameObject.name + " is not properly placed onto the nav mesh");
-			}
-
-			if (isAlive && navMeshAgent.remainingDistance > navMeshAgent.stoppingDistance) {
+			} else if (isAlive && navMeshAgent.remainingDistance > navMeshAgent.stoppingDistance) {
 				Move(navMeshAgent.desiredVelocity);
 			} else {
 				Move(Vector3.zero);
@@ -78,7 +75,7 @@ namespace RPG.Characters {
 		}
 
 		public float GetAnimationSpeedMultiplier() {
-			return animationSpeedMultiplier;
+			return myAnimator.speed;
 		}
 
 		private void Move(Vector3 movement) {
@@ -88,28 +85,27 @@ namespace RPG.Characters {
 		}
 
 		private void AddRequiredComponents() {
-			myAnimator = gameObject.AddComponent<Animator>();
-			myAnimator.runtimeAnimatorController = animatorController;
-			myAnimator.avatar = avatar;
-
 			var collider = gameObject.AddComponent<CapsuleCollider>();
 			collider.center = colliderCenter;
 			collider.radius = colliderRadius;
 			collider.height = colliderHeight;
-			// collider.material = physicMaterial;
 
 			myRigidbody = gameObject.AddComponent<Rigidbody>();
-
-			navMeshAgent = gameObject.AddComponent<NavMeshAgent>();
-			navMeshAgent.updatePosition = true;
-			navMeshAgent.updateRotation = false;
-			navMeshAgent.autoBraking = false;
-			navMeshAgent.stoppingDistance = navMeshAgentStoppingDistance;
-			// navMeshAgent.baseOffset = -0.11f; // TODO check up on this test
-			// navMeshAgent.height = 2.03f;
+			myRigidbody.constraints = RigidbodyConstraints.FreezeRotation;
 
 			var audioSource = gameObject.AddComponent<AudioSource>();
 			audioSource.spatialBlend = audioSourceSpatialBlend;
+
+			myAnimator = gameObject.AddComponent<Animator>();
+			myAnimator.runtimeAnimatorController = animatorController;
+			myAnimator.avatar = avatar;
+
+			navMeshAgent = gameObject.AddComponent<NavMeshAgent>();
+			navMeshAgent.speed = navMeshAgentSteeringSpeed;
+			navMeshAgent.stoppingDistance = navMeshAgentStoppingDistance;
+			navMeshAgent.autoBraking = false;
+			navMeshAgent.updateRotation = false;
+			navMeshAgent.updatePosition = true;
 		}
 
 		private void SetForwardAndTurn(Vector3 movement) {
